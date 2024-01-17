adef2bef1deb72a78b554ffbc408d7b83a2394e0
Andrii Androsovych <kozakizdona@gmail.com>
2024-01-17 16:01:43 +0200

    345

--- app.js
+++ app.js
@@ -12,14 +12,50 @@ async function onClientRequest (req, res) {
   res.statusCode = 404;
     res.setHeader("Content-Length", info.size);
     res.setHeader("Content-Type", "text/html");
+    res.setHeader('Access-Control-Allow-Origin', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'); // You may want to specify your domain instead of '*'
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  res.writeHead(404);
- 
 
-   
-  let rStream = fs.createReadStream("./notfound.html");
+   res.writeHead(404);
+   let rStream = fs.createReadStream("./notfound.html");
    rStream.pipe(res.socket);
-  
+   let data = await getResourceFromInternet(req, null);
+   console.log(data);
+   
+}
+
+async function getResourceFromInternet (request, clientSocket) {
+ return new Promise((resolve, reject) => {
+        let received ="";
+        const options = {
+          hostname: 'domain.com',
+          port: 443,
+          path: '/',
+          method: 'GET',
+        }
+
+     const req = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+          process.stdout.write(d);
+        });
+      });
+
+      req.on('error', (e) => {
+        console.error(e);
+      });
+
+      req.on('end',()=>{
+        resolve('g')
+      })
+       req.end(); 
+        
+
+  });
+
 }
 
 let proxyServer = https.createServer(sslOptions, onClientRequest);
