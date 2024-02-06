93af28ccb86808f22f44fad49e90f7d230d4c9ab
Andrii Androsovych <kozakizdona@gmail.com>
2024-02-06 16:00:25 +0200

    kdkdv

--- app.js
+++ app.js
@@ -8,22 +8,44 @@ let server = net.createServer(onConnect);
 
 server.listen(80, ()=>console.log("Listn on 80..."));
 
-function onConnect(socket){
+function onConnect(clientSocket){
   let clientData = '';
-  socket.on("data", (data)=>{
+  //one time listener -    We need only the data once, the starting packet
+  clientSocket.once("data", (data)=>{
+    
     clientData += data;
+    //parsing - throws into strings
     let strings = clientData.split('\r\n');
+    //extract host string
     let hostString = strings.find((x)=>/Host:/.test(x));
+    //vahe of "host" header:
     let hostValue = /^Host: (.+)$/.exec(hostString);
+    //the host value:
     hostValue = hostValue[1];
+    //searching port:
+    const portMatch = hostValue.match(/:(\d+)/);
+    // If port exists, extract it; otherwise, assign default port 80
+    const port = portMatch ? portMatch[1] : "443";
+    //extracting host:
+    const hostMatch = hostValue.match(/^([^:]+)(?::\d+)?/);
+    // Extracting the matched host name
+    const extractedHostName = hostMatch ? hostMatch[1] : hostValue;
+    console.log(`incoming message ${Date.now().toString()}, ${extractedHostName}`);
     //connect to remote host:
-    console.log(`incoming...${Date.now()}`);
-    //
-    net.createConnection(
-  let hash = Date.now().toString();
-  let msg = `HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:${hash.length}\r\n\r\n${hash}\r\n`;
-  socket.write(msg);
-  socket.end();
+   let aimServer= net.createConnection({
+      host:extractedHostName,
+      port:port,
+    },()=>{
+        //response to client - when https 
+        clientSocket.write('HTTP/1.1 200 OK\r\n\n');
+        // Piping the sockets
+        clientSocket.pipe(aimServer);
+        aimServer.pipe(clientSocket);
+    })
+  //let hash = Date.now().toString();
+  //let msg = `HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:${hash.length}\r\n\r\n${hash}\r\n`;
+  //clientSocket.write(msg);
+  //clientSocket.end();
   })
 
 
